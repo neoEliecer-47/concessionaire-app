79fbc3f84aa1dc293397833bdc9b95528479e884
Eliecer <eliecer.sanchez47@gmail.com>
2024-12-07 14:24:38 -0400

    types added to the button

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -3,17 +3,18 @@
 import Image from "next/image";
 import classNames from "classnames";
 import styles from './CustomButton.module.css'
+import { CustomButtonProps } from "../../types";
 
-const CustomButton = () => {
+const CustomButton = ( { title, containerStyles, handleClick }: CustomButtonProps ) => {
   return (
     <button
       disabled={false}
       type="button"
-      className={classNames('custom-btn')}
-      onClick={() => {}}
+      className={classNames('custom-btn', containerStyles)}
+      onClick={handleClick}
     >
         <span className={classNames(styles.spanButton)}>
-            title
+            {title}
         </span>
     </button>
   );
