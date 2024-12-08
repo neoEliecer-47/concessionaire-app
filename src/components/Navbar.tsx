8154ffb9b2e757feb1b117819f819fcbef1173cc
Eliecer <eliecer.sanchez47@gmail.com>
2024-12-08 00:25:20 -0400

    navbar firsts details added and also logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import Link from "next/link"
+import Image from "next/image"
+import CustomButton from "./CustomButton"
+import styles from './Navbar.module.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+   <header className={styles.headerContainer}>
+    <nav className={styles.navBox}>
+        <Link href='/' className={styles.linkBox}>
+            <Image 
+                src='/logo.svg'
+                alt="Car Hub Logo"
+                width={118}
+                height={18}
+                style={{ objectFit: 'contain' }}    
+            />
+        </Link>
+    </nav>
+   </header>
   )
 }
 
