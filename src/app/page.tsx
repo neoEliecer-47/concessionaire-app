ee83ee21b140d13cf8aedeb3328ed539c8e04891
Eliecer <eliecer.sanchez47@gmail.com>
2024-12-09 19:59:35 -0400

    cars container started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,9 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <Hero />
+      <div className={styles.carsContainer} id='discover'>
+        <div></div>
+      </div>
     </main>
   )
 }
