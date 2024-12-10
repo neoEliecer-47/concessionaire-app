ef78718fa9cbf4889e85cb5e458ed164c933681f
Eliecer <eliecer.sanchez47@gmail.com>
2024-12-09 22:26:07 -0400

    building form and filters as well as types for components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,29 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Hero from '@/components/Hero'
+import Image from "next/image";
+import styles from "./page.module.css";
+import Hero from "@/components/Hero";
+import SearchBar from "@/components/SearchBar";
+import CustomFilter from "@/components/CustomFilter";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Hero />
-      <div className={styles.carsContainer} id='discover'>
-        <div></div>
+
+      <div className={styles.carsContainer} id="discover">
+        <div className="home__text-container">
+          <h1 className={styles.titleCarsHome}>Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+
+        <div className="home__filters">
+          <SearchBar />
+
+          <div className="home__filter-container">
+            <CustomFilter title='fuel'/>
+            <CustomFilter title='year'/>
+          </div>
+        </div>
       </div>
     </main>
-  )
+  );
 }
--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,18 @@
 import { MouseEventHandler } from "react"
+import { manufacturers } from "../constants";
 
 export type CustomButtonProps = {
     title: string,
     containerStyles?: string,
     btnType?: "button" | "submit",
     handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export type CustomFilterProps = {
+    title: string
+}
+
+export type SearchManufacturerProps = {
+    manufacturer: string,
+    setManufacturer: (manufacturer: string) => void
 }
