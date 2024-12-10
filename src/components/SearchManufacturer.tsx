592eacb1ff3a4d21b66b94ac1fdb5aa0be2ad1c1
Eliecer <eliecer.sanchez47@gmail.com>
2024-12-10 13:52:36 -0400

    manufaterer input added and styled

--- src/components/SearchManufacturer.tsx
+++ src/components/SearchManufacturer.tsx
@@ -1,9 +1,24 @@
+'use client'
+
 import { SearchManufacturerProps } from "../../types"
+import { Combobox, Transition, ComboboxButton, ComboboxInput } from "@headlessui/react"
+import Image from "next/image"
 
 
 const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacturerProps) => {
   return (
-    <div>SearchManufacturer</div>
+    <div className="search-manufacturer">
+      <Combobox>
+        <div style={{ position: 'relative', width: '100%' }}>
+          <Combobox.Button style={{ position: 'absolute', top: '14px', left: '10px', border: 'none' }}>
+            <Image src='/car-logo.svg' alt="car logo" width={20} height={20}/>
+          </Combobox.Button>
+          <ComboboxInput className='search-manufacturer__input' placeholder="Volkswagen" style={{ paddingLeft: '2.5rem' }}/>
+
+          
+        </div>
+      </Combobox>
+    </div>
   )
 }
 
