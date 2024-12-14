8eeb7e5c76e113a0be2a3ae89868b351f6655a61
Eliecer <eliecer.sanchez47@gmail.com>
2024-12-14 19:22:01 -0400

    logic for car rent added

--- src/components/CarCard.tsx
+++ src/components/CarCard.tsx
@@ -7,8 +7,16 @@ type CarCardProps = {
 const CarCard = ({ car }: CarCardProps) => {
    const { city_mpg, year, make, model, transmission, drive } = car
     return (
-        <div className="car-card group">
+        <div className="car-card">
+          <div className="car-card__content">
+          <h2 className="car-card__content-title">
+            {make} - {model}
+          </h2>
+          </div>
 
+          <p>
+            <span>Car Rent...</span>
+          </p>
         </div>
     )
 };
--- utils/index.ts
+++ utils/index.ts
@@ -19,3 +19,17 @@ export async function fetchCars() {
     console.log(error.message);
   }
 }
+
+
+export function calculateCarRent(city_mpg: number, year: number) {
+  const basePricePerDay = 50
+  const mileageFactor = 0.1
+  const ageFactor = 0.05
+
+  const mileageRate = city_mpg * mileageFactor
+  const ageRate = (new Date().getFullYear() - year) * ageFactor
+
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate
+
+  return rentalRatePerDay
+}
