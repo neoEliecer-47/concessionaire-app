16ac2f72b2ff7509dd1186dee55abf071cbf4cfb
Eliecer <eliecer.sanchez47@gmail.com>
2024-12-15 01:03:25 -0400

    styles added and car rent function

--- src/components/CarCard.tsx
+++ src/components/CarCard.tsx
@@ -1,24 +1,60 @@
+import Image from "next/image";
 import { CarProps } from "../../types";
+import { calculateCarRent } from "../../utils";
 
 type CarCardProps = {
   car: CarProps;
 };
 
 const CarCard = ({ car }: CarCardProps) => {
-   const { city_mpg, year, make, model, transmission, drive } = car
-    return (
-        <div className="car-card">
-          <div className="car-card__content">
-          <h2 className="car-card__content-title">
-            {make} - {model}
-          </h2>
-          </div>
+  const { city_mpg, year, make, model, transmission, drive } = car;
+  const carRent = calculateCarRent(city_mpg, year);
 
-          <p>
-            <span>Car Rent...</span>
-          </p>
-        </div>
-    )
+  return (
+    <div className="car-card">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} - {model}
+        </h2>
+      </div>
+
+      <p
+        style={{
+          display: "flex",
+          marginTop: "1.5rem",
+          fontSize: "32px",
+          fontWeight: "800",
+        }}
+      >
+        <span
+          style={{
+            alignSelf: "flex-start",
+            fontSize: "14px",
+            fontWeight: "600",
+          }}
+        >
+          $
+        </span>
+          {carRent}
+
+        <span
+          style={{
+            alignSelf: "flex-end",
+            fontSize: "14px",
+            fontWeight: "500",
+          }}
+        >
+          
+          /day
+        </span>
+      </p>
+
+
+      <div style={{ position: 'relative', width: '100%', height: '10rem', margin: '0.75rem 0', objectFit: 'contain' }}>
+          <Image src='/hero.png' alt="car model" width={110} height={80}/>
+      </div>
+    </div>
+  );
 };
 
 export default CarCard;
--- utils/index.ts
+++ utils/index.ts
@@ -20,16 +20,15 @@ export async function fetchCars() {
   }
 }
 
-
 export function calculateCarRent(city_mpg: number, year: number) {
-  const basePricePerDay = 50
-  const mileageFactor = 0.1
-  const ageFactor = 0.05
+  const basePricePerDay = 50;
+  const mileageFactor = 0.1;
+  const ageFactor = 0.05;
 
-  const mileageRate = city_mpg * mileageFactor
-  const ageRate = (new Date().getFullYear() - year) * ageFactor
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor; //here we get the value calculated regarding the car's age and its facgtor minus our current year
 
-  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
-  return rentalRatePerDay
+  return rentalRatePerDay.toFixed(0);
 }
