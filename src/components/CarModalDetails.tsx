2156d99e43aa2c64bc1a3c27a449c54c4fe9245b
Eliecer <eliecer.sanchez47@gmail.com>
2024-12-19 00:28:31 -0400

    description images added

--- src/components/CarModalDetails.tsx
+++ src/components/CarModalDetails.tsx
@@ -53,13 +53,13 @@ const CarModalDetails = ({ isOpen, car, onClick }: CarModalDetailsProps) => {
                 position: "absolute",
                 right: "15px",
                 top: "10px",
-                backgroundColor: 'rgba(252, 252, 252, 0.6)',
-                border: 'none',
-                borderRadius: '9999px',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                padding: '5px 5px'
+                backgroundColor: "rgba(252, 252, 252, 0.6)",
+                border: "none",
+                borderRadius: "9999px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                padding: "5px 5px",
               }}
               onClick={handleCloseModal}
               type="button"
@@ -69,7 +69,7 @@ const CarModalDetails = ({ isOpen, car, onClick }: CarModalDetailsProps) => {
                 alt="close"
                 width={20}
                 height={20}
-                style={{ objectFit: "contain", padding: "0", opacity: '1' }}
+                style={{ objectFit: "contain", padding: "0", opacity: "1" }}
               />
             </button>
             <Image
@@ -79,6 +79,18 @@ const CarModalDetails = ({ isOpen, car, onClick }: CarModalDetailsProps) => {
               priority
               style={{ objectFit: "contain" }}
             />
+          </div>
+          <div className={styles.descriptionImagesContainer}>
+            <div className={styles.descriptionImagesContainerTwo}>
+              <Image src='/hero.png' alt="car" fill priority style={{ objectFit: 'contain' }} />
+            </div>
+            <div className={styles.descriptionImagesContainerTwo}>
+              <Image src='/hero.png' alt="car" fill priority style={{ objectFit: 'contain' }} />
+            </div>
+            <div className={styles.descriptionImagesContainerTwo}>
+              <Image src='/hero.png' alt="car" fill priority style={{ objectFit: 'contain' }} />
+            </div>
+
           </div>
 
           <div>
