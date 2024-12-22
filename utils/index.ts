9f25f28608ff1289b814cab8d77142d295c59c47
Eliecer <eliecer.sanchez47@gmail.com>
2024-12-22 09:52:08 -0400

    studio imagin aoi for cars models and detail images

--- utils/index.ts
+++ utils/index.ts
@@ -41,10 +41,24 @@ export function calculateCarRent(city_mpg: number, year: number) {
   return rentalRatePerDay.toFixed(0);
 }
 
-export function generateCarImageUrl(query: string) {
-  client.photos.search({ query, per_page: 3 }).then((photos) => {
-    console.log(photos);
-    return photos;    });
+export function generateCarImageUrl(car:  CarProps, angle?: string) {
+  
+  const url =  new URL('https://cdn.imagin.studio/getimage')
+
+  const { make, year, model } = car
+  
+  url.searchParams.append('customer', 'hrjavascript-mastery')
+  url.searchParams.append('make', make)
+  url.searchParams.append('modelFamily', model.split(' ')[0])
+  url.searchParams.append('zoomType', 'fullscreen')
+  url.searchParams.append('modelYear', `${year}`)
+  url.searchParams.append('angle', `${angle}`)
+  
+  return `${url}`
+  
+  // client.photos.search({ query, per_page: 3 }).then((photos) => {
+  //   console.log(photos);
+  //   return photos;    });
 }
 
 //pexels api key
